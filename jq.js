8645e60295b3dd70a41b3864ecd87e4e40e2f3d4
Fishyang1103 <wer1655213@hotmail.com>
2022-01-08 23:53:16 +0800

    AVENUE - Template RWD

---
+++ jq.js
@@ -0,0 +1,10 @@
+$('.content4').mouseenter(function () {
+  $('.chefs-special').css('transform', 'translate(-10px,-10px)')
+  $('.chefs-special').css('transform', 'rotate(10deg)')
+})
+
+$('.content4').mouseleave(function () {
+  $('.chefs-special').css('transform', ' translate(10px,10px)')
+  $('.chefs-special').css('transform', 'rotate(0deg)')
+})
+
